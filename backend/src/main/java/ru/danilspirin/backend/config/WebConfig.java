77dbdc497b827f284f75253d31256b85cd32a413
Danil <danilkaspirin@gmail.com>
2024-01-18 18:46:57 +1000

    feat(backend): add cors origins

---
+++ backend/src/main/java/ru/danilspirin/backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ru.danilspirin.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    // Отключение (разрешение всем) Cors Policy
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
